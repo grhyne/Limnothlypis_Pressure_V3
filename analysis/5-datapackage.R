77e4c22053f3aa965f9e447aa82f589799260403
Garrett Rhyne <70720203+grhyne@users.noreply.github.com>
2025-02-17 10:42:04 -0500

    Adjust column names, update DP script

--- analysis/5-datapackage.R
+++ analysis/5-datapackage.R
@@ -1,4 +1,8 @@
+# install.packages("pak")
+#pak::pkg_install("Rafnuss/GeoLocatoR")
 library(GeoLocatoR)
+library(zen4R)
+library(frictionless)
 
 ## Publish Data Pacakge
 # See https://raphaelnussbaumer.com/GeoLocatoR/articles/create-from-geopressuretemplate.html
@@ -10,17 +14,27 @@ pkg <- create_gldp_geopressuretemplate(".")
 pkg$title <- "GeoLocator Data Package: Swainson's Warbler"
 # pkg$embargo <- "2030-01-01"
 pkg$keywords <- c("Swainson's Warbler", "Neotropical Migrant", "multi-sensor geolocator")
+#Contributors / creators
+pkg$contributors <- list( # required
+  list(
+    title = "Garrett Rhyne",
+    roles = c("ContactPerson", "DataCurator", "ProjectLeader"),
+    email = "grhyne@abcbirds.org",
+    path = "https://orcid.org/0000-0003-4318-7840",
+    organization = "American Bird Conservancy"
+  )
+)
 # Add DOI of the datapackage if already available or reserve it https://help.zenodo.org/docs/deposit/describe-records/reserve-doi/#reserve-doi
-pkg$id <- "https://doi.org/"
+pkg$id <- "https://doi.org/10.5281/zenodo.14883861"
 # Provide the recommended citation for the package
-pkg$citation <- " Rhyne, G. (2025). GeoLocator Data Package: Swainson's Warbler [Data set]. Zenodo. https://doi.org/10.5281/zenodo.14606625"
+pkg$citation <- " Rhyne, G. (2025). GeoLocator Data Package: Swainson's Warbler [Data set]. Zenodo. https://doi.org/10.5281/zenodo.14883861"
 # Funding sources
- pkg$grants <- c("Louisiana State University")
+pkg$grants <- c("Louisiana State University")
 # Identifiers of resources related to the package (e.g. papers, project pages, derived datasets, APIs, etc.).
 pkg$relatedIdentifiers <- list(
   list(
     relationType="IsPartOf",
-    relatedIdentifier="10.5281/zenodo.14606625",
+    relatedIdentifier="10.5281/zenodo.14883861",
     relatedIdentifierType="DOI"
   ),
   list(
@@ -39,7 +53,8 @@ pkg <- pkg %>% add_gldp_geopressuretemplate(directory = ".", from = "interim")
 # print(pkg)
 
 # Check package
-check_gldp(pkg)
+plot(pkg)
+validate_gldp(pkg)
 
 # Write datapackage
 dir.create("data/datapackage", showWarnings = FALSE)
